cbd4dede3fd517f33b409e306db4dbaab4fc30c1
i-nigma <125734589+nNigma@users.noreply.github.com>
2024-07-31 19:29:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='parallel_load',  # Название вашей библиотеки
-    version='0.1.0',  # Версия вашей библиотеки
-    packages=find_packages(),  # Автоматически находит пакеты
-    install_requires=[  # Зависимости, если есть
-        # 'numpy',  # Пример зависимости
-    ],
-    author='nigma',  # Ваше имя
-    author_email='nigma.st@gmail.com',  # Ваш email
-    description='Библиотека позволяет загружать данные из ClickHouse и сохранять их в Parquet',
-    url='https://github.com/nNigma/my_libos',  # URL вашего репозитория
+   name='parallel_load',
+   version='0.1.0',
+   packages=find_packages(),
+   install_requires=[],
+   author='nigma',
+   author_email='nigma.st@gmail.com',
+   description='Библиотека позволяет загружать данные из ClickHouse и сохранять их в Parquet',
+   url='https://github.com/nNigma/my_libos',
 )
