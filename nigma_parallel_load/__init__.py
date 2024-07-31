2faf9240e0b0b304409cdc454641d6644bd631c1
i-nigma <125734589+nNigma@users.noreply.github.com>
2024-07-31 19:28:29 +0300

    Update __init__.py

--- nigma_parallel_load/__init__.py
+++ nigma_parallel_load/__init__.py
@@ -1,6 +1,1 @@
-# nigma_parallel_load/__init__.py
-
-# Можно импортировать функции или классы из этого модуля
-from .your_module import nigma_parallel_load  # Импорт функции из вашего модуля
-
-__all__ = ['nigma_parallel_load']  # Определяет, что будет доступно при импорте подмодуля
+     from .parallel_load import nigma_parallel_load
